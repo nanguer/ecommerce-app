8e59748dc132f4703487f8184cfc61477cf12a3d
Nahuel Nicolas Gimenez (WT65-IT ServicesPolandsp.z o.o) <NA20050369@wipro.com>
2020-02-25 15:27:15 +0100

    clean appbody

---
+++ Readme.md
@@ -0,0 +1,3 @@
+**_ eCommerce App basic boilerplate _**
+
+Basic components and dummy data for basic CRUD operations against a json file.
--- src/App.js
+++ src/App.js
@@ -2,12 +2,26 @@
 import React from "react";
 import { hot } from "react-hot-loader";
 // eslint-disable-next-line import/no-named-as-default
-import AppBody from "./components/AppBody";
+import { Container, AppBar, Typography } from "@material-ui/core";
+import ProductShowcase from "./components/ProductShowcase";
 
 import "./styles/styles.scss";
 
 const App = () => {
-  return <AppBody />;
+  return (
+    <>
+      <AppBar position="static">
+        <Typography variant="h6" color="inherit">
+          Photos
+        </Typography>
+      </AppBar>
+      <Container maxWidth="lg">
+        <h1>React - Redux eCommerce Application Boilerplate</h1>
+
+        <ProductShowcase />
+      </Container>
+    </>
+  );
 };
 
 export default hot(module)(App);
